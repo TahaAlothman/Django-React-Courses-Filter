b7faabf4606fc10cac4678f29d91f4720c04b26e
[TahaAlothman] <tahaaborawi@gmail.com>
2024-01-21 19:13:11 +0100

    create class Courses in models

--- Backend/project/courses/models.py
+++ Backend/project/courses/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=200)
+    description = models.TextField(max_length=3000)
+    image = models.ImageField(upload_to='courses')
+    price = models.IntegerField()
