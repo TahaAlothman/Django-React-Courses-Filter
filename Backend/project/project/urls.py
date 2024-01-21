948c44e8fd85ac912d423fe0d079996bab610029
[TahaAlothman] <tahaaborawi@gmail.com>
2024-01-21 20:03:03 +0100

    add Course List API & Category List API

--- Backend/project/project/urls.py
+++ Backend/project/project/urls.py
@@ -23,6 +23,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from courses.views import CourseListFilterAPI,CategoryListAPI
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -38,6 +39,8 @@ schema_view = get_schema_view(
 )
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('courses/api',CourseListFilterAPI.as_view()),
+    path('category/api',CategoryListAPI.as_view()),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
